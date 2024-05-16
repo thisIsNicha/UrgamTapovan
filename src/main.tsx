660ecee27a07f7e01d9cb414b8dcd0e3e05f191f
DivyanshMittal-exe <78041366+DivyanshMittal-exe@users.noreply.github.com>
2024-05-16 11:17:38 +0530

    Fixing deploy

--- src/main.tsx
+++ src/main.tsx
@@ -4,51 +4,48 @@ import "@fortawesome/fontawesome-free/css/all.min.css";
 import ReactDOM from 'react-dom/client'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
 import './index.css'
-// import Root from './routes/root'
-// import NotFound from './pages/Error/NotFound';
+import Root from './routes/root'
+import NotFound from './pages/Error/NotFound';
 import Home from './pages/Home/Home';
-// import HandLoom from './pages/Handloom/Handloom';
+import HandLoom from './pages/Handloom/Handloom';
 
-// import App from './App.tsx'
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Root />,
-//     errorElement: <NotFound />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <Home />,
-//       },
-//       {
-//         path: "/handloom",
-//         element: <HandLoom />,
-//       }
-//       // {
-//       //   path: "/handloom",
-//       //   element: <HandLoom />,
-//       // },
-//       // {
-//       //   path: "/components",
-//       //   element: <Components />,
-//       // },
-//     ],
-//   },
-// ]);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/handloom",
+        element: <HandLoom />,
+      }
+      // {
+      //   path: "/handloom",
+      //   element: <HandLoom />,
+      // },
+      // {
+      //   path: "/components",
+      //   element: <Components />,
+      // },
+    ],
+  },
+]);
 
 const rootElement = document.getElementById("root");
 if (rootElement) {
   ReactDOM.createRoot(rootElement)?.render(
     <React.StrictMode>
-      {/* <RouterProvider router={router} /> */}
-      <Home />
+      <RouterProvider router={router} />
     </React.StrictMode>
   );
 }
